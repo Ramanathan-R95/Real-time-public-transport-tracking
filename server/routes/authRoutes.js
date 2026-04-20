4d40d01b8dfe77c5670d911ab2fc4469974ed41c
Beesto <ramarajendran9585@gmail.com>
2026-04-20 10:58:57 +0530

    fix register errror

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const router           = require('express').Router();
 const { login, register } = require('../controllers/authController');
 
-router.post('/login', login);
+router.post('/login',    login);
 router.post('/register', register);
 
 module.exports = router;
