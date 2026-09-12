6bbda54125ab69c09b5460017bfd29a803ebfdb4
Beesto <ramarajendran9585@gmail.com>
2026-09-12 11:29:42 +0530

    fixing bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   optimizeDeps: {
     exclude: ['maplibre-gl'],
   },
   server: {
     port: 5173,
+    host: '0.0.0.0',
     proxy: {
       '/api':  'http://localhost:5000',
       '/sse':  'http://localhost:5000',
